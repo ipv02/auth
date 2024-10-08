50f47de00653b03b911b2657b3b8258a8c5c6e6f
Igor P-V <i_pogiba@tochka.com>
2024-10-08 21:26:07 +0300

    edit server main

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/ipv02/auth
 go 1.23
 
 require (
+	github.com/brianvoe/gofakeit v3.18.0+incompatible
 	github.com/fatih/color v1.17.0
 	google.golang.org/grpc v1.67.1
 	google.golang.org/protobuf v1.35.1
--- grpc/server/main.go
+++ grpc/server/main.go
@@ -1,9 +1,77 @@
 package main
 
-/*
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/brianvoe/gofakeit"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/types/known/emptypb"
+	"google.golang.org/protobuf/types/known/timestamppb"
+
+	desc "github.com/ipv02/auth/grpc/pkg/user_v1"
+)
+
 const grpcPort = 50051
 
 type server struct {
 	desc.UnimplementedUserV1Server
 }
-*/
+
+// Create ...
+func (s *server) Create(ctx context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
+	log.Printf("CreateRequest - Name: %s, Email: %s", req.GetName(), req.GetEmail())
+
+	return &desc.CreateResponse{
+		Id: 1,
+	}, nil
+}
+
+// Get ...
+func (s *server) Get(ctx context.Context, req *desc.GetRequest) (*desc.GetResponse, error) {
+	log.Printf("Note id: %d", req.GetId())
+
+	return &desc.GetResponse{
+		Id:        req.GetId(),
+		Name:      gofakeit.Name(),
+		Email:     gofakeit.Email(),
+		Role:      desc.UserRole_user,
+		CreatedAt: timestamppb.New(gofakeit.Date()),
+		UpdatedAt: timestamppb.New(gofakeit.Date()),
+	}, nil
+}
+
+// Update ...
+func (s *server) Update(ctx context.Context, req *desc.UpdateRequest) (*emptypb.Empty, error) {
+	log.Printf("UpdateRequest - ID: %d, Name: %s, Email: %s", req.GetId(), req.GetName(), req.GetEmail())
+
+	res := &emptypb.Empty{}
+	return res, nil
+}
+
+func (s *server) Delete(ctx context.Context, req *desc.DeleteRequest) (*emptypb.Empty, error) {
+	log.Printf("Deleting object with ID: %d", req.GetId())
+
+	res := &emptypb.Empty{}
+	return res, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+	desc.RegisterUserV1Server(s, &server{})
+
+	log.Printf("server listening at %v", lis.Addr())
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
